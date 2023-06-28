74e5647aeccab89fe377e7908c4ef1476bcac930
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-28 16:41:08 -0400

    Add functionality in 'main.js' for the 'Remove' buttons.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+const btnsRemove = document.querySelectorAll('.btn-remove');
 
+btnsRemove.forEach((button) => {
+	button.addEventListener('click', (event) => {
+		event.target.parentNode.parentNode.parentNode.remove();
+	});
+});
