ff2d2ed3ce2d1c27f0cc00b8ff95e69947d6987a
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-29 15:21:38 -0400

    Add 'Stock', 'Gluten' buttons with appropriate messages upon clicking.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,40 @@ btnsRemove.forEach((button) => {
 	});
 });
 
+// The user should have a way to update the "In Stock" value for existing resources. An interaction should change the resource from being "In Stock" to "Out Of Stock" and vice versa.
+const btnsStock = document.querySelectorAll('.btn-stock');
+
+btnsStock.forEach((button) => {
+	button.addEventListener('click', (event) => {
+		if (event.target.nextSibling.textContent === 'In Stock') {
+			event.target.nextSibling.classList.add('stock-out');
+			event.target.nextSibling.classList.remove('stock-in');
+			event.target.nextSibling.textContent = 'Out Of Stock';
+		} else {
+			event.target.nextSibling.classList.add('stock-in');
+			event.target.nextSibling.classList.remove('stock-out');
+			event.target.nextSibling.textContent = 'In Stock';
+		}
+	});
+});
+
+// The user should have a way to update the "Has Gluten" value for existing resources. An interaction should change the resource from being "Has Gluten" to "Gluten-Free" and vice versa.
+const btnsGluten = document.querySelectorAll('.btn-gluten');
+
+btnsGluten.forEach((button) => {
+	button.addEventListener('click', (event) => {
+		if (event.target.nextSibling.textContent === 'Has Gluten') {
+			event.target.nextSibling.classList.add('gluten-free');
+			event.target.nextSibling.classList.remove('gluten-has');
+			event.target.nextSibling.textContent = 'Gluten-Free';
+		} else {
+			event.target.nextSibling.classList.add('gluten-has');
+			event.target.nextSibling.classList.remove('gluten-free');
+			event.target.nextSibling.textContent = 'Has Gluten';
+		}
+	});
+});
+
 // Footer
 // Get the current year and `append` it to the copyright line in the `footer`.
 const currentYear = document.querySelector('#current-year');
