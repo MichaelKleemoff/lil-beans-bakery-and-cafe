a8243993521b08f2652c2671ceaf567fbab2b309
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-07-01 18:40:44 -0400

    Make all the 'buttons' functionality work. Each new product is added at the top of the inventory.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-// Form and Inventory
+// Accessing necessary nodes
 const form = document.querySelector('form');
 const inventory = document.querySelector('#inventory');
+const inventoryHr = document.querySelector('#inventory hr');
 
+// Form submission
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 
@@ -11,7 +13,7 @@ form.addEventListener('submit', (event) => {
 	const productStock = document.querySelector('#product-stock').value;
 	const productGluten = document.querySelector('#product-gluten').value;
 
-	const article = createNewProduct(
+	const newProduct = createNewProduct(
 		productName,
 		imageURL,
 		productPrice,
@@ -19,52 +21,39 @@ form.addEventListener('submit', (event) => {
 		productGluten
 	);
 
-	inventory.append(article);
+	inventoryHr.insertAdjacentElement('afterend', newProduct);
 
 	form.reset();
 });
 
-// When a user clicks on the "Remove" button for each resource, that resource is removed from the page.
-const btnsRemove = document.querySelectorAll('.btn-remove');
-
-btnsRemove.forEach((button) => {
-	button.addEventListener('click', (event) => {
-		event.target.parentNode.parentNode.parentNode.remove();
-	});
-});
-
-// The user should have a way to update the "In Stock" value for existing resources. An interaction should change the resource from being "In Stock" to "Out Of Stock" and vice versa.
-const btnsStock = document.querySelectorAll('.btn-stock');
-
-btnsStock.forEach((button) => {
-	button.addEventListener('click', (event) => {
+// Buttons functionality
+inventory.addEventListener('click', (event) => {
+	// The user should have a way to update the "In Stock" value for existing resources. An interaction should change the resource from being "In Stock" to "Out Of Stock" and vice versa.
+	if (event.target.className === 'btn-stock') {
 		if (event.target.nextSibling.textContent === 'In Stock') {
-			event.target.nextSibling.classList.add('stock-out');
-			event.target.nextSibling.classList.remove('stock-in');
 			event.target.nextSibling.textContent = 'Out Of Stock';
+			event.target.nextSibling.className = 'stock-out';
 		} else {
-			event.target.nextSibling.classList.add('stock-in');
-			event.target.nextSibling.classList.remove('stock-out');
 			event.target.nextSibling.textContent = 'In Stock';
+			event.target.nextSibling.className = 'stock-in';
 		}
-	});
-});
+	}
 
-// The user should have a way to update the "Has Gluten" value for existing resources. An interaction should change the resource from being "Has Gluten" to "Gluten-Free" and vice versa.
-const btnsGluten = document.querySelectorAll('.btn-gluten');
-
-btnsGluten.forEach((button) => {
-	button.addEventListener('click', (event) => {
+	// The user should have a way to update the "Has Gluten" value for existing resources. An interaction should change the resource from being "Has Gluten" to "Gluten-Free" and vice versa.
+	if (event.target.className === 'btn-gluten') {
 		if (event.target.nextSibling.textContent === 'Has Gluten') {
-			event.target.nextSibling.classList.add('gluten-free');
-			event.target.nextSibling.classList.remove('gluten-has');
 			event.target.nextSibling.textContent = 'Gluten-Free';
+			event.target.nextSibling.className = 'gluten-free';
 		} else {
-			event.target.nextSibling.classList.add('gluten-has');
-			event.target.nextSibling.classList.remove('gluten-free');
 			event.target.nextSibling.textContent = 'Has Gluten';
+			event.target.nextSibling.className = 'gluten-has';
 		}
-	});
+	}
+
+	// When a user clicks on the "Remove" button for each resource, that resource is removed from the page.
+	if (event.target.className === 'btn-remove') {
+		event.target.parentNode.parentNode.parentNode.remove();
+	}
 });
 
 // Footer
